231e2edd4c08e5c1ef2425ed7215aa9976844160
coopman66 <38082324+coopman66@users.noreply.github.com>
2022-07-28 10:31:20 -0600

    Fixed Filename bug
    Fixed bug that occurs when destination folder doesn't have max number of backup directories

--- bashscripts/rsync_incremental.sh
+++ bashscripts/rsync_incremental.sh
@@ -15,12 +15,20 @@ cd /backup
 # rotate backups
 # wildcard allows for missing days due to downtime (using async scheduler)
 rm -rf backup.*.${backup_backlog}
-#3 condition for loop needed because of bash script limitations
 for (( log_number=$backup_backlog; log_number >= 2; log_number-- )); do
-    log_date=$(ls | grep "backup.*.$((${log_number}-1))" | cut -d'.' -f 2)
-    mv backup.${log_date}.$(( ${log_number} - 1 )) backup.${log_date}.${log_number}
+    log_date=$(ls | grep backup.*.$((${log_number}-1)) | cut -d'.' -f 2)
+    # If the destination folder doesn't have the max number of backups (backup_backlog), filenaming gets weird
+    # This makes sure that if any of the backup directories are missing, an empty file with the right name is added
+    # New empty file keeps naming convention consistent
+    if [ ! -d "backup.${log_date}.$(( ${log_number} - 1 ))" ]; then
+        touch "backup.$(date --date="$(date) -${log_number} days" '+%F').$(( ${log_number} - 1 ))"
+        log_date=$(ls | grep backup.*.$((${log_number}-1)) | cut -d'.' -f 2)
+    fi
+    mv "backup.${log_date}.$(( ${log_number} - 1 ))" "backup.${log_date}.${log_number}"
 done
-cp -al backup.$(ls | grep "backup.*.0" | cut -d'.' -f 2).0 backup.$(ls | grep "backup.*.1" | cut -d'.' -f 2).1
+cp -al "backup.$(ls | grep backup.*.0 | cut -d'.' -f 2).0" "backup.$(ls | grep backup.*.0 | cut -d'.' -f 2).1"
 
-#make new backup
-rsync -a --delete ${to_backup[@]} "backup.$(date '+%F').0/"
+# make new backup
+rsync -a --dry-run --delete --exclude="/home/*/.cache" ${to_backup[@]} "backup.$(ls | grep backup.*.0 | cut -d'.' -f 2).0"
+# Keeps naming convention consistent
+mv "backup.$(ls | grep backup.*.0 | cut -d'.' -f 2).0" "backup.$(date '+%F').0/"
